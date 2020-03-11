1cfde22d5d54c07f4b8e4c5d1ed77f1775703142
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-03-11 19:33:56 +0900

    gulp-sass 인스톨 및 초기세팅

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,11 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import gimage from "gulp-image";
+import sass from "gulp-sass";
 
-const routes = { 
+sass.complier = require("node-sass")
+
+const routes = {
     pug: {
         watch: "src/**/*.pug",
         src: "src/*.pug",
@@ -13,6 +16,11 @@ const routes = {
     img: {
         src: "src/img/*",
         dest: "build/img"
+    },
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css"
     }
 }
 
@@ -30,20 +38,28 @@ const webserver = () =>
         open: true
     }));
 
-const watch = () => {
-    gulp.watch(routes.pug.watch, pug);
-    //gulp.watch(routes.img.src, img);
-};
-
 const img = () =>
     gulp
     .src(routes.img.src)
     .pipe(gimage())
     .pipe(gulp.dest(routes.img.dest));
 
+const styles = () => 
+    gulp
+    .src(routes.scss.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(routes.scss.dest))
+
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.scss.watch, styles)
+    //gulp.watch(routes.img.src, img);
+};
+
+
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const postDev = gulp.parallel([webserver, watch])
 
